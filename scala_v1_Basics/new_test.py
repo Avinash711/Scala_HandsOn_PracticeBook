25e5e24f696a4f10eb5700755b32ae9734313a12
AllAboutCoding@Avinash <48080428+Avinash711@users.noreply.github.com>
2022-01-26 19:15:04 +0530

    m7

--- scala_v1_Basics/new_test.py
+++ scala_v1_Basics/new_test.py
@@ -1,2 +1,9 @@
 print("I am from the new test script")
 print('python is super cool!!!')
+
+class PythonCool:
+  def __init__(self):
+    print("I am cooler than you!!")
+  @staticmethod
+  def static_method():
+    print('I am the utility')
